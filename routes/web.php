2e76981e642467d5784e55ab84791b2a69ec305b
MariaMartinezRos <cibernuggets@gmail.com>
2024-10-03 16:58:48 +0200

    dia 03/10/24, inicializacion del proyecto

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/','welcome');
+Route::view('contacto','contact');
+Route::view('/blog','blog');
+Route::view('/nosotros','about');
+
+Route::match(['put', 'patch'], '/', function(){
+    return 'This is a PUT PATCH request';
 });
 
 Route::get('/dashboard', function () {
