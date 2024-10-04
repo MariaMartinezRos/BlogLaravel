aaa6145543dcc8138e3ee75ca732ef2183bc1cf4
MariaMartinezRos <cibernuggets@gmail.com>
2024-10-04 16:52:35 +0200

    segunda update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--@component('components.layout')--}}
 
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Inicio</title>
+{{--@endcomponent--}}
 
 
-    </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <ul>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('blog') }}">Blog</a></li>
-            <li><a href="{{ route('about') }}">Nosotros</a></li>
-            <li><a href="{{ route('contact') }}">Contacto</a></li>
-        </ul>
-       <h1>Hola</h1>
-    </body>
-</html>
+<x-layout meta-title="Inicio" meta-description="Descripcion de la página de inicio">
+    <h1>Inicio</h1>
+    <x-slot:sidebar>
+        <p>Home Sidebar</p>
+    </x-slot:sidebar>
+</x-layout>
