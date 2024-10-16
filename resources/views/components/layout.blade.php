2708806ba891a8567591eaf348c8dbacaf8294dd
MariaMartinezRos <cibernuggets@gmail.com>
2024-10-16 17:17:13 +0200

    vistas con tailwind. npm run build y basicamente decoracion

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $metaTitle ?? 'Default title' }}</title>
     <meta name="description" content="{{ $metaDescription ?? 'Default Description' }}">
-
+    @vite(['resources/js/app.js','resources/css/app.css'])
 </head>
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<body class="flex h-screen flex-col bg-slate-100 selection:bg-sky-600 selection:text-sky-50 dark:bg-slate-950">
 <x-partials.navigation/>
 
 {{--mostrar mensajes flash de tipo estatus--}}
@@ -19,14 +19,18 @@
 </div>
 @endsession
 
-{{ $slot }}
+<main class="flex-1 p-4">
+    {{ $slot }}
+</main>
 
-@isset($sidebar)
-    <div id="sidebar">
-        <h3>Sidebar</h3>
-        {{ $sidebar }}
-    </div>
-@endif
+
+
+{{--@isset($sidebar)--}}
+{{--    <div id="sidebar">--}}
+{{--        <h3>Sidebar</h3>--}}
+{{--        {{ $sidebar }}--}}
+{{--    </div>--}}
+{{--@endif--}}
 </body>
 </html>
 
