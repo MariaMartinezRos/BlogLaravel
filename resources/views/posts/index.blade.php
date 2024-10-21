9e7f14b743c1c240dc98ef6b6cc4759b3b0b70da
MariaMartinezRos <cibernuggets@gmail.com>
2024-10-21 17:23:40 +0200

    CORRECIÓN DE ERRORES PT.1

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,8 +14,12 @@
         {{--        decoración css--}}
         @auth
         <div class="flex items-center justify-center">
+{{--            <a--}}
+{{--                href="#"--}}
+{{--                class="group rounded-full bg-sky-600 p-2 text-sky-100 shadow-lg duration-300 hover:bg-sky-700 active:bg-sky-800"--}}
+{{--            >--}}
             <a
-                href="#"
+                href="{{ route('posts.create') }}"
                 class="group rounded-full bg-sky-600 p-2 text-sky-100 shadow-lg duration-300 hover:bg-sky-700 active:bg-sky-800"
             >
                 <svg
@@ -37,25 +41,84 @@
             </a>
         </div>
         @endauth
+
+        <div
+            class="mx-auto mt-8 grid max-w-6xl gap-4 md:grid-cols-2 lg:grid-cols-3"
+        >
+            @foreach($posts as $post)
+                <article
+                    class="flex flex-col overflow-hidden rounded bg-white shadow dark:bg-slate-900"
+                >
+                    {{--<div class="h-52">
+                            <a
+                                class="duration-300 hover:opacity-75"
+                                href="/article.html"
+                            >
+                                <img
+                                    class="h-full w-full object-cover object-center"
+                                    src="/img/article-1.jpg"
+                                    alt="Boost your conversion rate"
+                                />
+                            </a>
+                        </div>--}}
+                    <div class="flex-1 space-y-3 p-5">
+                        {{--<h3 class="text-sm font-semibold text-sky-500">
+                            Desk and Office
+                        </h3>--}}
+                        <h2
+                            class="text-xl font-semibold leading-tight text-slate-800 dark:text-slate-200"
+                        >
+                            <a class="hover:underline" href="{{ route('posts.show', $post) }}">
+                                {{ $post->title }}
+                            </a>
+                        </h2>
+                        <p
+                            class="hidden text-slate-500 dark:text-slate-400 md:block"
+                        >
+                            {{ $post->body }}
+                        </p>
+                    </div>
+                    {{--<div class="flex space-x-2 p-5">
+                        <img
+                            class="h-10 w-10 rounded-full"
+                            src="https://ui-avatars.com/api?name=Roel Aufderehar"
+                            alt="Roel Aufderehar"
+                        />
+                        <div class="flex flex-col justify-center">
+                            <span
+                                class="text-sm font-semibold leading-4 text-slate-600 dark:text-slate-400"
+                            >
+                                Roel Aufderehar
+                            </span>
+                            <span class="text-sm text-slate-500">
+                                    Mar 16, 2023
+                            </span>
+                        </div>
+                    </div>--}}
+                </article>
+            @endforeach
+    </div>
     </div>
 
 
-    <a href="{{route('posts.create')}}">{{__('Create a new Post')}}</a>
-    @foreach($posts as $post)
-        <div style="display: flex; align-items: baseline">
-            <h2>
-                <a href="{{route('posts.show',$post)}}">{{ $post->title }}</a>
-            </h2>
 
-            <a href="{{ route('posts.edit', $post) }}">Edit</a>
 
-            <form action="{{ route('posts.destroy',$post) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Delete</button>
-            </form>
-        </div>
-    @endforeach
+{{--    <a href="{{route('posts.create')}}">{{__('Create a new Post')}}</a>--}}
+{{--    @foreach($posts as $post)--}}
+{{--        <div style="display: flex; align-items: baseline">--}}
+{{--            <h2>--}}
+{{--                <a href="{{route('posts.show',$post)}}">{{ $post->title }}</a>--}}
+{{--            </h2>--}}
+
+{{--            <a href="{{ route('posts.edit', $post) }}">Edit</a>--}}
+
+{{--            <form action="{{ route('posts.destroy',$post) }}" method="POST">--}}
+{{--                @csrf--}}
+{{--                @method('DELETE')--}}
+{{--                <button type="submit">Delete</button>--}}
+{{--            </form>--}}
+{{--        </div>--}}
+{{--    @endforeach--}}
 </x-layout>
 
 
