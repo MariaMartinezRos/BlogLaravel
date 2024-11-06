ecb93ec9772b0599dea93771ce1ec5a749253d2f
MariaMartinezRos <cibernuggets@gmail.com>
2024-11-06 16:26:21 +0100

    ejercicio 3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+//        User $user = User::
+
+        User::factory(count:5)->create();
+        $users = User::all();
+
+        $users->each(function ($user) {
+           $user->posts()->saveMany(
+               Post::factory(count:10)->make() // Crea 10 posts por cada usuario
+           );
+        });
+
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
     }
 }
